a52d474930faecee569ab13b7c8874960b99ee2f
Punch <dachawatsathon.kunweeroj@agoda.com>
2022-08-09 11:31:21 +0700

    init

---
+++ data/skills.ts
@@ -0,0 +1,30 @@
+
+interface Skill {
+  category: string
+  detail: string
+}
+
+const skills: Skill[] = [
+  {
+    category: "Languages",
+    detail: "JS es7+, TS, .Net, .Net Core, SQL, HTML, CSS"
+  },
+  {
+    category: "Frameworks",
+    detail: "ReactJS, React Native, .Net WebApi, .Net App, Express, Typeorm"
+  },
+  {
+    category: "Libraries",
+    detail: "clsx, Axios, json-server, jest, csv-parser, Redux, Recoil, Antd, date-fns, , moment-js,styled-components, lodash, storybook etc..."
+  },
+  {
+    category: "DevOps",
+    detail: "Gitlab CI/CD, Docker, Docker-compose, Kubenetes"
+  },
+  {
+    category: "Runtime",
+    detail: "NodeJS"
+  }
+]
+
+export default skills
