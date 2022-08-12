a0e13cfe352dee69bf7b23ab3a5d4c438b2e075e
Punch <dachawatsathon.kunweeroj@agoda.com>
2022-08-12 14:09:56 +0700

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,88 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import careerData from '../data/careerSummary'
-import skills from '../data/skills'
-import contacts from '../data/contacts'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Dachawatsathon</title>
-        <meta name="description" content="Dachawatsathon resume." />
+        <title>Palette Website</title>
+        <meta name="description" content="Palette Website." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <section className={styles.section}>
           <h1 className={styles.title}>
-            Dachawatsathon Kunweeroj
+            Palette Website
           </h1>
           <h2 className={styles["sub-title"]}>
-            Fullstack Software Developer
+          Palette Website.
           </h2>
-          <p>
-            I'm a fullstack software engineer with robust problem-solving skills, fast learner, responsible, eager to learn new technologies.
-          </p>
-          {contacts.map(contact => (
-            <>
-            <div className={styles["contact-row"]}>
-              <p>
-                {contact.logo}
-              </p>
-              <p>
-                {contact.detail}
-              </p>
-            </div>
-            </>
-          ))}
-        </section>
-
-        <section className={styles.section}>
-          <div className={styles["career-container"]}>
-            <h1 className={styles["career-title"]}>
-              Career Summary
-            </h1>
-            
-            {careerData.map((_careerData, index) => (
-              <>
-                <h2 className={styles["career-date"]}>
-                  {_careerData.date}
-                </h2>
-                <h3 className={styles["career-date"]}>
-                  {_careerData.title}
-                </h3>
-                <h4 className={styles["career-date"]}>
-                  {_careerData.company}
-                </h4>
-                <ul>
-                  {_careerData.tasks.map((task) => (
-                    <li>
-                      {task}
-                    </li>
-                  ))}
-                </ul>
-              </>
-            ))}
-          </div>
-        </section>
-
-        <section className={styles.section}>
-          <h1 className={styles["skill-title"]}>
-            Skills
-          </h1>
-          
-          {skills.map((skill) => (
-            <>
-              <h2 className={styles["career-date"]}>
-                {skill.category}
-              </h2>
-              <p className={styles["skill-detail"]}>
-                {skill.detail}
-              </p>
-            </>
-          ))}
         </section>
         
       </main>
